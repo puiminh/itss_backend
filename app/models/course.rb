664e566be663d49026f83f3afc871245c2fb2885
vutuankiet4599 <vutuankiet4599@gmail.com>
2023-03-03 19:25:25 +0700

    fix delete course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,25 +1,22 @@
 class Course < ApplicationRecord
     belongs_to :author, class_name: 'User'
     # 1-n 
-    has_many :vocabularies
+    has_many :vocabularies, :dependent => :delete_all
 
     # n-n through collections_courses -> it's dump name by minh captain
-    has_many :collections_courses
+    has_many :collections_courses, :dependent => :delete_all
     # has_many :collections, through: :collections_courses
 
-    has_many :comments
+    has_many :comments, :dependent => :delete_all
     # has_many :users, through: :comments
 
-    has_many :ratings
+    has_many :ratings, :dependent => :delete_all
     # has_many :users, through: :ratings
 
-    has_many :progresses
+    has_many :progresses, :dependent => :delete_all
     # has_many :vocabularies, through: :progresses
     # has_many :users, through: :progresses
 
-    has_many :bookmark_collections
-    # has_many :users, through: :bookmark_collections
-
-    has_many :bookmark_courses
+    has_many :bookmark_courses, :dependent => :delete_all
     # has_many :users, through: :bookmark_courses
 end
