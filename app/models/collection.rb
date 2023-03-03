e0521f1c2ae5257c24d3737d0e34c7820f8a74f7
puiminh <volmicrolab@gmail.com>
2023-03-03 20:19:30 +0700

    delete collection and all collection dependent

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,8 +1,8 @@
 class Collection < ApplicationRecord
-    has_many :collections_courses
+    has_many :collections_courses, :dependent => :delete_all
     # has_many :courses, through: :collections_courses
 
-    has_many :bookmark_collections
+    has_many :bookmark_collections, :dependent => :delete_all
     # has_many :users, through: :bookmark_collections
 
     belongs_to :author, class_name: 'User'
